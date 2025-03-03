8766da75747c4613fa5d47b8c9954ba69345a491
fhafshin <fhafshin@gmail.com>
2025-03-04 00:37:31 +0330

    rep

--- src/main/java/ir/project/springcourse/SpringCourseApplication.java
+++ src/main/java/ir/project/springcourse/SpringCourseApplication.java
@@ -1,13 +1,22 @@
 package ir.project.springcourse;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"ir.project.springcourse","ir.project.two"})
-public class SpringCourseApplication {
+public class SpringCourseApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCourseApplication.class, args);
     }
 
+@Bean
+    public CommandLineRunner commandLineRunner() {
+
+        return runner->{
+            System.out.println("hello world");
+        };
+}
 }
